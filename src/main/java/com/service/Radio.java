eb09841f73c9040049dda597d4815d9f11631bb0
Mikhail Dukhno <ratercorp@gmail.com>
2023-06-29 15:21:28 +0300

    add Radio.java

---
+++ src/main/java/com/service/Radio.java
@@ -0,0 +1,42 @@
+package com.service;
+public class Radio {
+    private int radioStationNumber;
+    private int currentVolume;
+    private final int maxRadioStation = 10;
+    public int getVolume() {
+        return currentVolume;
+    }
+
+    public void setVolume(int newCurrentVolume) {
+        if (newCurrentVolume >= 0 && newCurrentVolume <= 100) {
+            currentVolume = newCurrentVolume;
+        }
+
+    }
+
+    public void increaseVolume() {
+        setVolume(currentVolume + 1);
+    }
+
+    public void decreaseVolume() {
+        setVolume(currentVolume - 1);
+    }
+
+    public int getRadioStationNumber() {
+        return radioStationNumber;
+    }
+
+    public void setRadioStationNumber(int newRadioStationNumber) {
+        if (newRadioStationNumber >= 0 && newRadioStationNumber < maxRadioStation) {
+            radioStationNumber = newRadioStationNumber;
+        }
+    }
+
+    public void next(){
+        setRadioStationNumber((radioStationNumber + 1) % maxRadioStation);
+    }
+
+    public void prev() {
+        setRadioStationNumber(((radioStationNumber - 1) + maxRadioStation) % maxRadioStation);
+    }
+}
---
+++ src/test/java/com/service/RadioTest.java
@@ -0,0 +1,90 @@
+package com.service;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class RadioTest {
+    @ParameterizedTest
+    @CsvSource({"100, 100",
+            "0, 101",
+            "70, 70",
+            "0, 0",
+            "0, -1"
+    })
+    public void setVolume(int expected, int newCurrentVolume) {
+        Radio radio = new Radio();
+        radio.setVolume(newCurrentVolume);
+        int actual = radio.getVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"1, 0",
+                "2, 1",
+                "100, 99",
+                "100, 100",
+    })
+    public void increaseVolumeTest(int expected, int newCurrentVolume) {
+        Radio radio = new Radio();
+        radio.setVolume(newCurrentVolume);
+        radio.increaseVolume();
+        int actual = radio.getVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"0, 0",
+                "0, 1",
+                "99, 100",
+                "98, 99",
+    })
+    public void decreaseVolumeTest(int expected, int newCurrentVolume) {
+        Radio radio = new Radio();
+        radio.setVolume(newCurrentVolume);
+        radio.decreaseVolume();
+        int actual = radio.getVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"0, 0",
+                "1, 1",
+                "9, 9",
+                "0, 10",
+                "0, -1"
+    })
+    public void setRadioStationNumberTest(int expected, int newRadioStationNumber) {
+        Radio radio = new Radio();
+        radio.setRadioStationNumber(newRadioStationNumber);
+        int actual = radio.getRadioStationNumber();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"1, 0",
+                "2, 1",
+                "0, 9",
+                "9, 8"
+    })
+    public void nextTest(int expected, int newRadioStationNumber) {
+        Radio radio = new Radio();
+        radio.setRadioStationNumber(newRadioStationNumber);
+        radio.next();
+        int actual = radio.getRadioStationNumber();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"9, 0",
+            "0, 1",
+            "8, 9",
+            "7, 8",
+    })
+    public void prevTest(int expected, int newRadioStationNumber) {
+        Radio radio = new Radio();
+        radio.setRadioStationNumber(newRadioStationNumber);
+        radio.prev();
+        int actual = radio.getRadioStationNumber();
+        Assertions.assertEquals(expected, actual);
+    }
+}
